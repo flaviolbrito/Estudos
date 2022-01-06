76473a3a76ef9a23af292feecc98aa06c50d956f
flaviolbrito <flaviolbrito@gmail.com>
2022-01-06 19:05:07 -0300

    Teste Enumeration  - serve para andar sobre as coleções

--- JavaPlatform/java04/src/colecoes/Enumeration/TesteEnumeration.java
+++ JavaPlatform/java04/src/colecoes/Enumeration/TesteEnumeration.java
@@ -8,25 +8,26 @@ public class TesteEnumeration {
 	/**
 	 * @param args
 	 */
+	//TesteEnumeration - // Enumeration serve para andar sobre as coleções
 	public static void main(String[] args) {
 
 		Hashtable<Integer, String> nomes = new Hashtable<Integer, String>();
 
-		nomes.put(1, "Maria");
+		nomes.put(1, "Maria"); // hash integer@492
 		nomes.put(2, "José");
 		nomes.put(3, "Henrique");
 		nomes.put(4, "Matias");
 		nomes.put(5, "Nascimento");
 		nomes.put(6, "Maria");
-		nomes.put(1, "Maria");
+		nomes.put(1, "Maria"); // hash integer@492
 
-		System.out.println(nomes);
-		System.out.println(nomes.get(2));
-
+		System.out.println("Todos os nomes: " + nomes);
+		System.out.println("Posicao 2: " + nomes.get(2));
+         // Enumeration serve para andar sobre as coleções
 		Enumeration<String> enumeracao = nomes.elements();
 
 		while (enumeracao.hasMoreElements()) {
-			System.out.println(enumeracao.nextElement());
+			System.out.println("Nome: " + enumeracao.nextElement());
 		}
 
 	}
