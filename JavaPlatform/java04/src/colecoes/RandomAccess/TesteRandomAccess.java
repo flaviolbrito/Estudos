1492890679632d55dcc2bbd22113bdf1939565ff
flaviolbrito <flaviolbrito@gmail.com>
2022-01-07 12:22:23 -0300

    Teste Random Access - interface com acesso mais rapido para coleções

--- JavaPlatform/java04/src/colecoes/RandomAccess/TesteRandomAccess.java
+++ JavaPlatform/java04/src/colecoes/RandomAccess/TesteRandomAccess.java
@@ -2,6 +2,7 @@ package colecoes.RandomAccess;
 
 import java.util.ArrayList;
 
+// Teste Random Access - interface com acesso mais rapido para coleções.
 public class TesteRandomAccess {
 
 	/**
@@ -15,6 +16,7 @@ public class TesteRandomAccess {
 		listaString.add("b");
 		listaString.add("c");
 		listaString.add("d");
+
 		
 		for (int i = 0; i < listaString.size(); i++) {
 			System.out.println("listaString " + i + ": " + listaString.get(i));
