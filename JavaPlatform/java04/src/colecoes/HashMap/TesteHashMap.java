9ad61e93a556ecf6a83857e1cc62a5ab9103a619
flaviolbrito <flaviolbrito@gmail.com>
2022-01-07 10:12:00 -0300

    Teste HashMap - mapeamento de objetos no esquema chave/valor para pesquisa

--- JavaPlatform/java04/src/colecoes/HashMap/TesteHashMap.java
+++ JavaPlatform/java04/src/colecoes/HashMap/TesteHashMap.java
@@ -3,13 +3,13 @@ package colecoes.HashMap;
 import java.util.HashMap;
 import java.util.Map;
 
+//Teste HashMap - mapeamento de objetos no esquema chave/valor para pesquisa
 public class TesteHashMap {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//Teste HashMap - mapeamento de objetos no esquema chave/valor para pesquisa
 		Map<Integer, String> nomes = new HashMap<>();
 
 		nomes.put(1, "Maria");
