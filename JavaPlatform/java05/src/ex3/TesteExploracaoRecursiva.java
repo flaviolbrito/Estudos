47e23d17029b182749ca16b443f77c2155d295b9
flaviolbrito <46071926+flaviolbrito@users.noreply.github.com>
2022-01-18 17:33:46 -0300

    TesteExploracaoRecursiva - Lista Diretório e exibe todos os arquivos extensão ".java"

--- JavaPlatform/java05/src/ex3/TesteExploracaoRecursiva.java
+++ JavaPlatform/java05/src/ex3/TesteExploracaoRecursiva.java
@@ -2,14 +2,14 @@ package ex3;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
+// TesteExploracaoRecursiva - Lista Diretório e exibe todos os arquivos extensão ".java"
 public class TesteExploracaoRecursiva {
 
 	public static void main(String[] args) {
 
 		try {
 			// No Paths incluir o diretório desejado
-			Files.list(Paths.get("c:\\windows")).filter(p -> p.toString().endsWith(".java"))
+			Files.list(Paths.get("E:\\teste")).filter(p -> p.toString().endsWith(".java"))
 					.forEach(System.out::println);
 
 		} catch (Exception e) {
