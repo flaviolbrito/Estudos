c3ae79f9883f37cea09e58a3466e3df544efad7f
flaviolbrito <flaviolbrito@gmail.com>
2022-01-29 15:06:59 -0300

    update1

---
+++ Spring/42scj-cerveja-master/src/main/java/com/fiap/cerveja/dto/CervejaDTO.java
@@ -0,0 +1,45 @@
+package com.fiap.cerveja.dto;
+
+import com.fiap.cerveja.domain.Tipo;
+
+import java.time.LocalDate;
+
+public class CervejaDTO {
+
+    private Long id;
+    private String nome;
+    private Tipo tipo;
+    private LocalDate vencimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getVencimento() {
+        return vencimento;
+    }
+
+    public void setVencimento(LocalDate vencimento) {
+        this.vencimento = vencimento;
+    }
+}
--- Spring/cerveja/cerveja/src/main/java/com/fiap/cerveja/controller/CervejaController.java
+++ Spring/cerveja/cerveja/src/main/java/com/fiap/cerveja/controller/CervejaController.java
@@ -1,10 +1,10 @@
 package com.fiap.cerveja.controller;
 
 import com.fiap.cerveja.domain.Tipo;
+import com.fiap.cerveja.dto.CervejaCreateOrUpdateDTO;
 import com.fiap.cerveja.dto.CervejaDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -30,16 +30,43 @@ public class CervejaController {
         cervejaDTO2.setVencimento(LocalDate.of(2022, 2, 15));
 
         return Arrays.asList(cervejaDT01, cervejaDTO2);
+    }
+     @GetMapping
+    public List<CervejaDTO> buscarCervejas(
+
+
+
+     )
+        return cervejaList.stream()
+
 
     }
+    @GetMapping("{idCerveja}")
+    public CervejaDTO buscarCervejaPorId(
+        @PathVariable(value = "idCerveja") Long id
+        ){
+        return cervejaList.stream()
+                .filter(cervejaDTO -> cervejaDTO.getId().equals(id))
+                .findFirst()
+                .orElseThrow(()-> new ResponseStatusException(HttpSatus.BAD_REQUEST,"cerveja não encotrada"));
+                ;
+
+    }
+
+    @PostMapping
+    public CervejaDTO criar (
+            @RequestBody CervejaCreateOrUpdateDTO createOrUpdateDTO
+            ){
+      cervejaDTO cervejaDTO =  new CervejaDTO();
+      cerveja.DTO setId(cervejaList.size() + 1L);
+      cervejaDTO.setNome(createOrUpdateDTO.getNome());
+      cervejaList.add(cervejaDTO);
+      return  cervejaDTO;
+
+    }
+
+    )
+
 }
-    //@GetMapping
-    //public String buscarCervejas(){
-    //    return "Hello Word";
 
-   // public List<String> buscarCervejas(){
-   //     return Arrays.asList("Skol","Gracial");
-
-
-   // }
 
