71821e12efcb1b06abdeeabfbdc28f89a609cd5f
flaviolbrito <flaviolbrito@gmail.com>
2022-02-09 20:50:08 -0300

    Teste IO - Input Output Arquivos - interface FileVisitor - Copia arquivo

--- JavaPlatform/java05/src/ex2/TesteCopiaArquivos.java
+++ JavaPlatform/java05/src/ex2/TesteCopiaArquivos.java
@@ -6,13 +6,18 @@ import java.nio.file.Paths;
 
 public class TesteCopiaArquivos {
 
-	//Teste IO - Input Output Arquivos - interface FileVisitor
+	//Teste IO - Input Output Arquivos - interface FileVisitor - Copia arquivo
 	public static void main(String[] args) {
 
 		try {
-			
-			Path origem = Paths.get("C:\\Users\\flavi\\Downloads\\download.pdf");
-			Path destino = Paths.get("C:\\Users\\flavi\\Downloads\\copia");
+
+			// Windows
+			// Path origem = Paths.get("C:\\Users\\flavi\\Downloads\\slf4j-api-1.7.9.jar");
+			// Path destino = Paths.get("C:\\Users\\flavi\\Downloads\\copia");
+
+            //Mac
+			Path origem = Paths.get("/Users/flaviolbrito/Downloads/slf4j-api-1.7.9.jar");
+			Path destino = Paths.get("/Users/flaviolbrito/Downloads/copia");
 			Files.walkFileTree(origem, new CopiarArquivos(destino));
 
 		} catch (Exception e) {
