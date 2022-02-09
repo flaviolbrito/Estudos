f4994aa2217f44225c86f52f6aa85a58928fc2f0
flaviolbrito <flaviolbrito@gmail.com>
2022-02-09 20:53:16 -0300

    TesteExploracaoRecursiva - Lista Diretório e exibe todos os arquivos extensão ".zip"

--- JavaPlatform/java05/src/ex3/TesteExploracaoRecursiva.java
+++ JavaPlatform/java05/src/ex3/TesteExploracaoRecursiva.java
@@ -2,14 +2,18 @@ package ex3;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-// TesteExploracaoRecursiva - Lista Diretório e exibe todos os arquivos extensão ".java"
+// TesteExploracaoRecursiva - Lista Diretório e exibe todos os arquivos extensão ".zip"
 public class TesteExploracaoRecursiva {
 
 	public static void main(String[] args) {
 
 		try {
-			// No Paths incluir o diretório desejado
-			Files.list(Paths.get("E:\\teste")).filter(p -> p.toString().endsWith(".java"))
+			// Windows -  No Paths incluir o diretório desejado
+			// Files.list(Paths.get("E:\\teste")).filter(p -> p.toString().endsWith(".java"))
+			//		.forEach(System.out::println);
+
+			// MAC -  No Paths incluir o diretório desejado
+			Files.list(Paths.get("/Users/flaviolbrito/Downloads/")).filter(p -> p.toString().endsWith(".zip"))
 					.forEach(System.out::println);
 
 		} catch (Exception e) {
